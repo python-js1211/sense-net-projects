3e3ef4e1a1863fa8ee568e68b1e5f56c7ddd9afb
python-js1211 <bruceames59@gmail.com>
2020-09-01 15:36:59 -0500

    Add arguments to compiler

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 """Setup for package sensenet
 """
-import tensorflow as tf
+
+tf_ver = 'tensorflow>=2.3,<2.4'
+import_err = 'Cannot import tensorflow.  Please run `pip install %s`' % tf_ver
+
+try:
+    import tensorflow as tf
+except ModuleNotFoundError:
+    raise ImportError(import_err)
 
 from os import path
 from setuptools import setup, Extension, find_packages
@@ -13,12 +20,15 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), 'r') as f:
     long_description = f.read()
 
+compile_args = ['-std=c++11', '-fPIC'] + tf.sysconfig.get_compile_flags()
+
 tree_module = Extension(__tree_ext_prefix__,
                         define_macros = [('MAJOR_VERSION', '0'),
                                          ('MINOR_VERSION', '1')],
-                        include_dirs = [tf.sysconfig.get_include()],
-                        library_dirs = [tf.sysconfig.get_lib()],
-                        extra_compile_args=['-std=c++11'],
+                        include_dirs=[tf.sysconfig.get_include()],
+                        library_dirs=[tf.sysconfig.get_lib()],
+                        extra_compile_args=compile_args,
+                        extra_link_args=tf.sysconfig.get_link_flags(),
                         sources = ['cpp/tree_op.cc'])
 setup(
     name='bigml-sensenet',
@@ -38,5 +48,5 @@ setup(
     test_suite='nose.collector',
     install_requires=[
         'numpy>=1.18,<1.19',
-        'tensorflow>=2.3,<2.4',
+        tf_ver,
     ])
