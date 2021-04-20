b28f9b576d856a87e40e95cc5e7dccc25682b0f6
python-js1211 <bruceames59@gmail.com>
2021-04-20 14:31:30 -0500

    Factor out yolo output decoding

--- sensenet/layers/yolo.py
+++ sensenet/layers/yolo.py
@@ -5,59 +5,32 @@ from sensenet.accessors import get_image_shape
 from sensenet.layers.construct import LAYER_FUNCTIONS
 from sensenet.layers.utils import build_graph
 
-class YoloBranches():
-    def __init__(self, network, nclasses):
-        self._nclasses = nclasses
-        self._input_size = get_image_shape(network)[1]
-        self._branches = []
+def yolo_decode(self, features, decoding_info, input_size, nclasses):
+    strides, anchors, xyscale = decoding_info
+    fsize = tf.shape(features)[0]
+    osize = tf.constant(input_size // int(strides))
+    ans = len(anchors)
 
-        assert network['layers'][-1]['type'] == 'yolo_output_branches'
-        out_branches = network['layers'][-1]
+    feature_shape = (fsize, osize, osize, ans, 5 + nclasses)
+    conv_output = tf.reshape(features, feature_shape)
 
-        for i, branch in enumerate(out_branches['output_branches']):
-            idx = branch['input']
-            d_info = [branch[k] for k in ['strides', 'anchors', 'xyscale']]
-            layers = make_sequence(branch['convolution_path'], LAYER_FUNCTIONS)
+    all_outputs = tf.split(conv_output, (2, 2, 1, nclasses), axis=-1)
+    raw_dxdy, raw_dwdh, raw_conf, raw_prob = all_outputs
 
-            self._branches.append((idx, d_info, layers))
+    xy_grid = tf.meshgrid(tf.range(osize), tf.range(osize))
+    xy_grid = tf.expand_dims(tf.stack(xy_grid, axis=-1), axis=2)
+    xy_grid = tf.expand_dims(xy_grid, axis=0)
+    xy_grid = tf.cast(tf.tile(xy_grid, [fsize, 1, 1, ans, 1]), tf.float32)
 
-    def decode_outputs(self, features, decoding_info):
-        strides, anchors, xyscale = decoding_info
-        fsize = tf.shape(features)[0]
-        osize = tf.constant(self._input_size // int(strides))
-        ans = len(anchors)
+    xy_correction = (tf.sigmoid(raw_dxdy) * xyscale) - 0.5 * (xyscale - 1)
+    pred_xy = (xy_correction + xy_grid) * strides
+    pred_wh = tf.exp(raw_dwdh) * tf.constant(anchors, dtype=tf.float32)
+    pred_xywh = tf.concat([pred_xy, pred_wh], axis=-1)
 
-        feature_shape = (fsize, osize, osize, ans, 5 + self._nclasses)
-        conv_output = tf.reshape(features, feature_shape)
+    pred_conf = tf.sigmoid(raw_conf)
+    pred_prob = tf.sigmoid(raw_prob)
 
-        all_outputs = tf.split(conv_output, (2, 2, 1, self._nclasses), axis=-1)
-        raw_dxdy, raw_dwdh, raw_conf, raw_prob = all_outputs
-
-        xy_grid = tf.meshgrid(tf.range(osize), tf.range(osize))
-        xy_grid = tf.expand_dims(tf.stack(xy_grid, axis=-1), axis=2)
-        xy_grid = tf.expand_dims(xy_grid, axis=0)
-        xy_grid = tf.cast(tf.tile(xy_grid, [fsize, 1, 1, ans, 1]), tf.float32)
-
-        xy_correction = (tf.sigmoid(raw_dxdy) * xyscale) - 0.5 * (xyscale - 1)
-        pred_xy = (xy_correction + xy_grid) * strides
-        pred_wh = tf.exp(raw_dwdh) * tf.constant(anchors, dtype=tf.float32)
-        pred_xywh = tf.concat([pred_xy, pred_wh], axis=-1)
-
-        pred_conf = tf.sigmoid(raw_conf)
-        pred_prob = tf.sigmoid(raw_prob)
-
-        return conv_output, tf.concat([pred_xywh, pred_conf, pred_prob], axis=-1)
-
-    def __call__(self, inputs):
-        predictions = []
-
-        for i, decoding_info, layers in self._branches:
-            features = propagate(layers, inputs[i])
-            predicted_values = self.decode_outputs(features, decoding_info)
-
-            predictions.append(predicted_values)
-
-        return predictions
+    return conv_output, tf.concat([pred_xywh, pred_conf, pred_prob], axis=-1)
 
 class Yolo(YoloBranches):
     def __init__(self, network, nclasses):
@@ -70,6 +43,9 @@ class Yolo(YoloBranches):
         for branch in network['metadata']['outputs']:
             d_info = [branch[k] for k in ['strides', 'anchors', 'xyscale']]
             self._branches.append((branch['input'], d_info))
+
+    def decode_outputs(self, features, d_info):
+        return yolo_decode(features, d_info, self._input_size, self._nclasses)
 
     def __call__(self, inputs):
         predictions = []
